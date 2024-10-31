7b5e4b8dcebb37f097708335137be2bbc465391e
Mathew Joseph T A <144910668+mathewjosephta@users.noreply.github.com>
2024-10-31 08:48:24 +0530

    Create FloatMultiplication.java

---
+++ Basic-Programs /FloatMultiplication.java
@@ -0,0 +1,16 @@
+// Java Program to print Multiplication of two floating point Number.
+
+import java.io.*;
+public class FloatMultiplication{
+    Public static void main(String[] args){
+        float f1 = 1.5f;
+        float f2 = 2.0f;
+        float sum = f1*f2;
+        System.out.println("The Sum of Two Float Integers is : "+ sum);
+  }
+}
+
+//SAMPLE OUTPUT
+/*
+The Sum of Two Float Integers is : 3.0
+*/
