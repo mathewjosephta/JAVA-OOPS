23592b8b0469e1cdab3272458c43876816b7d941
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2024-11-10 07:07:26 +0530

    Create demo.java

---
+++ Classes-and-Objects /demo.java
@@ -0,0 +1,11 @@
+class Student{
+    String name;
+    int rollno;
+    
+    public static void main(String args[])
+    {
+        Student s1=new Student();
+        System.out.println(s1.name);
+        System.out.println(s1.rollno);
+    }
+}
